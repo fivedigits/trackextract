d59948e4b84ceede688fdf01570097edaa2b2075
Moritz Schönherr <moritz.schoenherr@gmail.com>
2016-11-09 11:31:44 +0100

    added argparse and some optimisations regarding needed disk space

--- trackextract.py
+++ trackextract.py
@@ -3,11 +3,14 @@
 import re
 import sys
 from subprocess import call
+import argparse
 
 
 def parseList(ls_file):
 
-    t_expr = re.compile('\d\d:\d\d:\d\d')
+    t_expr = re.compile('\d{0,2}\:?\d{1,2}:\d\d')
+
+    alpha = re.compile('[\w\s]+')
 
     f = open(ls_file,"r")
 
@@ -23,13 +26,15 @@ def parseList(ls_file):
 
         t_matches = t_expr.findall(line)
 
-        n_matches = t_expr.split(line)
+        rest = ''.join(t_expr.split(line))
+
+        name = ''.join(alpha.findall(rest))
 
         if len(t_matches) == 1:
 
             timestamps.append(t_matches[0])
             
-            names.append(n_matches[0].strip(" -,;.:"))
+            names.append(name.strip(" -,;.:\r\n"))
 
         else:
 
@@ -37,9 +42,19 @@ def parseList(ls_file):
 
     return {"time": timestamps, "name": names}
 
-def extractTracks(beg_ls,name_ls,input_file,artist,album,genre):
+def extractTracks(beg_ls,name_ls,args):
 
-    call(["ffmpeg","-i",input_file,input_file + ".wav"])
+    input_file = args.infile
+
+    artist = args.artist
+
+    album = args.album
+
+    genre = args.genre
+
+    print("Converting infile to ogg.")
+
+    call(["ffmpeg","-loglevel","quiet","-i",input_file,"-q","9",input_file + ".ogg"])
 
     if len(name_ls) != len(beg_ls):
 
@@ -54,29 +69,29 @@ def extractTracks(beg_ls,name_ls,input_file,artist,album,genre):
 
         print("Extracting: " + names[index])
 
-        call(["sox",input_file + ".wav",names[index] + ".wav","trim",beg_ls[index],end_ls[index]])
+        call(["sox",input_file + ".ogg","-C","6",names[index] + ".ogg","trim",beg_ls[index],end_ls[index]])
 
     for index in range(len(names)):
-
-        call(["ffmpeg","-i",names[index] + ".wav","-q","6", names[index] + ".ogg"])
 
         call(["normalize-ogg",names[index] + ".ogg"])
 
         call(["id3tag","--artist",artist,"--album",album,"--song",names[index],"--track",str(index+1),"--total",str(len(names)),"--genre",genre,names[index]+ ".ogg"])
 
-    call(["rm","*.wav"])
+    call(["rm",input_file + ".ogg"])
 
 if __name__ == "__main__":
 
-    input_file = sys.argv[1]
+    parser = argparse.ArgumentParser(description='Split a single audio file into multiple based on a list in a text file.')
 
-    ls_file = sys.argv[2]
+    parser.add_argument('infile',help="audio file to split")
+    parser.add_argument('listfile',help="list of tracks with timestamps [dd:h]h:mm, one per line")
+    parser.add_argument('-A',"--artist",default="Unknown Artist",help="Artist")
+    parser.add_argument('-a',"--album",default="Unknown Album",help="Album")
+    parser.add_argument('-g',"--genre",default="Unknown Genre",help="Genre")
 
-    artist = sys.argv[3]
+    args = parser.parse_args()
 
-    album = sys.argv[4]
-    
-    genre = sys.argv[5]
+    ls_file = args.listfile
 
     metadata = parseList(ls_file)
 
@@ -84,4 +99,4 @@ if __name__ == "__main__":
 
     names = metadata["name"]
 
-    extractTracks(beg_times, names,input_file,artist,album,genre)
+    extractTracks(beg_times, names,args)
